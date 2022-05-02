15d94a65598fa37c2d3e985c6c370a493d5e0136
Duc Le Tran <letrananhduc0311@gmail.com>
2022-05-02 21:57:42 +0200

    Set up timer for experiments

--- app/src/main/java/ducletran/tech/imutransformer/ui/datacollection/SensorDataCollectionScreen.kt
+++ app/src/main/java/ducletran/tech/imutransformer/ui/datacollection/SensorDataCollectionScreen.kt
@@ -1,7 +1,22 @@
 package ducletran.tech.imutransformer.ui.datacollection
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ducletran.tech.imutransformer.R
+import ducletran.tech.imutransformer.ui.theme.IMUTransformerTheme
+import ducletran.tech.imutransformer.utils.tickerFlow
+import kotlinx.coroutines.flow.collectLatest
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.round
+import kotlin.time.Duration.Companion.milliseconds
 
 @Composable
 fun SensorDataCollectionScreen(
@@ -10,5 +25,59 @@ fun SensorDataCollectionScreen(
     runningTime: Long,
     onRunFinished: () -> Unit
 ) {
+    var currentTime by remember { mutableStateOf(0.00) }
+    val isExperimentStarted = currentTime > preparationTime
+    val formattedExperimentTime = if (isExperimentStarted) {
+        val runningTimeInSeconds = runningTime / 1000
+        val currentTimeInSeconds = round((currentTime - preparationTime) / 1000)
+        "$currentTimeInSeconds/$runningTimeInSeconds"
+    } else {
+        val remainingTimeInSeconds = (preparationTime - currentTime) / 1000
+        val decimalFormat = DecimalFormat("#.##")
+        decimalFormat.roundingMode = RoundingMode.DOWN
+        decimalFormat.format(remainingTimeInSeconds)
+    }
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = if (isExperimentStarted) {
+                    stringResource(id = R.string.running_experiment)
+                } else {
+                    stringResource(id = R.string.experiment_starting)
+                }
+            )
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = formattedExperimentTime,
+                fontSize = 64.sp
+            )
+        }
+    }
 
+    LaunchedEffect(key1 = "time_flow", block = {
+        val tickingTime = 100
+        tickerFlow(tickingTime.milliseconds).collectLatest {
+            if (currentTime > preparationTime + runningTime) {
+                onRunFinished()
+            } else {
+                currentTime += tickingTime
+            }
+        }
+    })
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+private fun SensorDataCollectionScreenPreview() {
+    IMUTransformerTheme {
+        SensorDataCollectionScreen(
+            modifier = Modifier.fillMaxSize(),
+            preparationTime = 2_000L,
+            runningTime = 10_000L,
+            onRunFinished = { }
+        )
+    }
 }
---
+++ app/src/main/java/ducletran/tech/imutransformer/utils/TickerFlow.kt
@@ -0,0 +1,13 @@
+package ducletran.tech.imutransformer.utils
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlin.time.Duration
+
+fun tickerFlow(period: Duration, initialDelay: Duration = Duration.ZERO) = flow {
+    delay(initialDelay)
+    while (true) {
+        emit(Unit)
+        delay(period)
+    }
+}
